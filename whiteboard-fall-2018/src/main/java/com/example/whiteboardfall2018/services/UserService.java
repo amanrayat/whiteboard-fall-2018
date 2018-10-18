82eb9e08ff13170ab4335f0092abeea28d6bc217
amanrayat <amanrayat32@gmail.com>
2018-10-18 00:25:00 -0400

    Adding few userservice methods

--- whiteboard-fall-2018/src/main/java/com/example/whiteboardfall2018/services/UserService.java
+++ whiteboard-fall-2018/src/main/java/com/example/whiteboardfall2018/services/UserService.java
@@ -6,8 +6,10 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.whiteboardfall2018.models.Person;
@@ -23,14 +25,60 @@ public class UserService {
 		return person;
 	}
 
-	
+
 	@GetMapping("/api/profile")
 	public Person profile(HttpSession session) {
 		Person currentUser = (Person) session.getAttribute("currentUser");
 		for( Person person :pList) {
 			if(person.getId() == currentUser.getId()) return person;
 		}
-			return null;
+		return null;
 	}
+
+	@PostMapping("/api/login")
+	public Person login(@RequestBody Person user, HttpSession session) {
+		for(Person person : pList) {
+			if(person.getUserName()==user.getUserName() && person.getPassword() == user.getPassword()) {
+				session.setAttribute("currentUser", user);
+			}
+		}
+		return user;
+	}
+
+	@GetMapping("/api/logout")
+	public String invalidateSession(
+			HttpSession session) {
+		session.invalidate();
+		return "session invalidated";
+	}
+
+	@GetMapping("api/user")
+	public List<Person> findAllPersons(@RequestParam (name = "username" , required = false) String username){
+		if(username !=null) {
+			return pList;
+		}
+		else {
+			for(Person person : pList) {
+				if(person.getUserName() == username) {
+
+					return (List<Person>) person;
+				}
+			}
+		}
+		return null;
+
+	}
+
+	@GetMapping("api/user/{userId}")
+	public Person findUserById(@PathVariable ("userId") int userId){
+		for(Person person : pList) {
+			if(person.getId() == userId) {
+				return person;
+			}
+		}
+		return null;
+
+	}
+
 
 }
