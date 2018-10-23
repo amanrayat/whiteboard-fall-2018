41d97ed6061480ae86dfc96c135f1684d25d5812
amanrayat <amanrayat32@gmail.com>
2018-10-23 18:55:02 -0400

    adding few changes

--- scc/src/solution.java
+++ scc/src/solution.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class solution {
+
+    public static void main (String args[]){
+        for(int i = 1 ; i <=9 ; i++){
+            if(i%3==0){
+                System.out.println("(" + i + ", " + 0 + " )");
+            }
+            else{
+                System.out.print("(" + i + ", " + (i+1) + " )");
+            }
+        }
+    }
 }
